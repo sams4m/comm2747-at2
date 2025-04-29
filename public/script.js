9720f98f688da43a18bb535e8c4d606b24686a9a
Sam <kyj.samantha@gmail.com>
2025-04-29 15:11:32 +1000

    edit

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 import { drawStar } from "/drawStar.js";
+import { colours } from "./colour.js";
+import * as THREE from "/three.js";
 
 // document styling
 document.body.style.margin = 0;
@@ -11,10 +13,82 @@ cnv.height = window.innerHeight;
 
 const ctx = cnv.getContext(`2d`);
 
+// Set up scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x7252dc);
+const camera = new THREE.PerspectiveCamera(
+  70,
+  cnv.width / cnv.height,
+  0.01,
+  10
+);
+camera.position.z = 2;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const clock = new THREE.Clock();
+
+// Setup renderer
+// size of renderer
+renderer.setSize(window.innerWidth, window.innerHeight);
+// adding renderer to dom
+document.body.appendChild(renderer.domElement);
+
 // vars
 let particleArr = [];
-let starParticle;
+let coli = 0;
 
+// ----------------------------------------------------------------------- //
+// SHADER MATERIAL
+const shaderMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    u_time: { value: 0.0 },
+  },
+  vertexShader: `
+   uniform float u_time;
+   varying vec3 vNormal;
+   varying vec3 vPosition;
+   
+         void main() {
+           vNormal = normal;
+   
+           // Animate the vertices
+           vec3 newPosition = position;
+           float displacement = sin(position.y * 18.0 + u_time * 2.0) * 0.1;
+           newPosition += normal * displacement;
+   
+           vPosition = newPosition;
+           gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+         }
+   
+   `,
+  fragmentShader: `
+   uniform float u_time;
+   varying vec3 vNormal;
+   varying vec3 vPosition;
+   
+         void main() {
+           // Create a color based on the position and normal
+           // multiplying vpos and vec3 makes the pattern and phases more intense
+           // creating a moire effect
+           vec3 color = 1.0 + 1.5 * sin(u_time * vPosition * 2.0 * vec3(0, 5, 4));
+           // vec3 color = 1.0 - (((u_time % 9) % 1) + vPosition + vec3(0, 5, 4));
+   
+   
+           // Add some shading based on the normals
+           float lighting = dot(normalize(vNormal), normalize(vec3(1.0, 1.0, 1.0)));
+           lighting = 0.1 + lighting * 0.5;
+   
+           gl_FragColor = vec4(color * lighting, 1.0);
+         }
+   
+   `,
+  side: THREE.DoubleSide,
+});
+
+// Create plane that fills the screen
+const planeGeometry = new THREE.PlaneGeometry(2, 2);
+const plane = new THREE.Mesh(planeGeometry, shaderMaterial);
+scene.add(plane);
+// ----------------------------------------------------------------------- //
 // MOUSE OBJ; tracks mouse x, y coord
 // grab mouse position
 let mouse = {
@@ -32,7 +106,8 @@ window.addEventListener("mousemove", function (event) {
   mouse.y = event.y;
 });
 
-// CREATE PARTICLE CLASS
+// ----------------------------------------------------------------------- //
+// CLASS: PARTICLE
 class Particle {
   constructor(x, y, dirX, dirY, size, colour, npoint) {
     // x coordinate
@@ -130,7 +205,8 @@ class Particle {
   }
 }
 
-// FUNC TO RANDOMISE VALUES FOR PARTICLES
+// ----------------------------------------------------------------------- //
+// INIT: RANDOMISE VALUES FOR PARTICLES
 function init() {
   // number of particles
   let numOf = (cnv.height * cnv.width) / 8000;
@@ -150,7 +226,7 @@ function init() {
     let dirY = Math.random() * (1.5 + 2.5) - 2.5;
 
     // colour
-    let colour = "rgb(243, 249, 210)";
+    let colour = "#" + colours[coli];
 
     // number of points on star
     // random value between 7 and 15
@@ -162,9 +238,10 @@ function init() {
   }
 }
 
+// FUNC: CONNECT
 // checking if particles are close enough to connect
 function connect() {
-  let opacityVal = 0.7;
+  let opacityVal = 0.8;
   // go through every particle
   for (let a = 0; a < particleArr.length; a++) {
     // going through consecutive particles in array
@@ -178,7 +255,7 @@ function connect() {
       // the smaller the number, the longer the lines,
       // the more particles connected
       if (dist < (cnv.width / 5) * (cnv.height / 5)) {
-        opacityVal = 0.7 - dist / 5000;
+        opacityVal = 0.7 - dist / 7000;
         ctx.strokeStyle = "rgba(51, 65, 57," + opacityVal + ")";
         ctx.lineWidth = 1;
         //console.log(ctx.lineWidth);
@@ -191,22 +268,52 @@ function connect() {
   }
 }
 
+cnv.addEventListener("onclick", cnvClicked());
+
+function cnvClicked() {
+  console.log("screen clicked");
+  // change colour index
+  let newColi = Math.floor(Math.random() * colours.length);
+  // update coli
+  coli = newColi;
+  console.log(coli);
+
+  //init();
+}
+
+// ----------------------------------------------------------------------- //
+
+// // create a torus knot
+// const geometry = new THREE.TorusKnotGeometry(5, 4.7, 41, 15, 14, 4);
+// const mesh = new THREE.Mesh(geometry, shaderMaterial);
+// scene.add(mesh);
+
+// // add light
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambientLight);
+
 // ANIMATION LOOP
 function animate() {
-  requestAnimationFrame(animate);
   // BACKGROUND STYLE
   // clearing previous frame
-  ctx.clearRect(0, 0, innerWidth, innerHeight);
-  //ctx.fillStyle = "rgb(255 141 161)";
-  ctx.fillStyle = "rgb(183, 93, 105)";
-  ctx.fillRect(0, 0, innerWidth, innerHeight);
+  //   ctx.clearRect(0, 0, innerWidth, innerHeight);
+  //   //ctx.fillStyle = "rgb(255 141 161)";
+  //   ctx.fillStyle = "#7252DC";
+  //   ctx.fillRect(0, 0, innerWidth, innerHeight);
+
+  // shader update
+  shaderMaterial.uniforms.u_time.value = clock.getElapsedTime();
+  renderer.render(scene, camera);
 
   // update each star particle
   particleArr.forEach((e) => {
     e.update();
   });
 
+  // call the connect func to draw lines
   connect();
+
+  requestAnimationFrame(animate);
 }
 
 // call init fill array with randomised particles
@@ -214,11 +321,14 @@ init();
 // call animate
 animate();
 
+// ----------------------------------------------------------------------- //
 // web responsive
 onresize = () => {
   cnv.width = innerWidth;
   cnv.height = innerHeight;
   mouse.radius = (cnv.height / 200) * (cnv.width / 200);
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
   init();
 };
 
